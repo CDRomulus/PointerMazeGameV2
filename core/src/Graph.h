765b4c27b793cadfeaf2fa242eb4c2e67a325089
Rommulluss <rommulluss@protonmail.com>
2018-12-16 09:13:39 +0000

    Cleanup 03

--- core/src/Graph.h
+++ core/src/Graph.h
@@ -1,10 +1,75 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-#include "Node.h"
-#include "Edge.h"
+
 #include <vector>
 #include <iostream>
-#include <fstream>
 #include <random>
+
+class Node
+{
+	private:
+	std::string m_name;
+	std::vector<char> bearing;
+	std::vector<Node*> m_nodes;
+	
+	public:
+	Node(std::string &name)
+	{
+		this->m_name = name;
+		this->m_nodes.reserve(1);
+		std::cout<<"Name of Node = "<<m_name<<std::endl;
+	}
+	~Node()
+	{
+		for (unsigned int i=0; i<this->m_nodes.size();i++)
+		{
+			printf("Node Destructor Called");
+			delete &this->m_nodes[i];			
+		}
+	}
+};
+
+class Graph
+{
+	std::vector<Node*> m_map;
+	
+	public:
+	Graph()
+	{
+		GraphGeneration();
+	}
+	
+	~Graph()
+	{
+		printf("Graph Destructor Called\n");
+		for(unsigned i =0; i<m_map.size();i++)
+		{
+			
+			delete &(*m_map[i]);	
+			std::cout<<m_map[i]<<std::endl;
+		}
+		
+		
+		
+	}
+	private:
+	void GraphGeneration()
+	{
+		//array for name/n/e/s/w
+		//Parsing();
+		printf("GraphGeneration\n");
+		std::string x  = "A";
+		std::string y = "B";
+		Node* n = new Node(x);
+		Node* m = new Node(y);
+		
+		this->m_map.push_back(n);
+		this->m_map.push_back(m);
+		
+		
+	}
+	
+};
+
 
 #endif
--- core/src/Main.cpp
+++ core/src/Main.cpp
@@ -2,21 +2,7 @@
 
 int main()
 {
-	std::vector<int> ListOne;
-	printf("Hello World!");
-	std::cout<<"Numbers =\n";
-	ListOne.reserve(7);
-	ListOne.emplace_back(1); 
-	ListOne.emplace_back(2); 
-	ListOne.emplace_back(3); 
-	ListOne.emplace_back(4); 
-	ListOne.emplace_back(5); 
-	ListOne.emplace_back(6);
-    ListOne.emplace_back(ListOne[3]+ListOne[5]);  
-	for( unsigned int x=0; x<ListOne.size();x++)
-	{
-		std::cout<<ListOne[x]<<"\n";
-	}
+	Graph graph_one;
 	
 	std::cin.get();
 }
